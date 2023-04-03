123d4e8104281498fe9f6d6e10442bcb85aa5be2
Eve-Drake <111064993+Eve-Drake@users.noreply.github.com>
2023-04-03 11:32:20 +0100

    first commit

---
+++ dice_roll.py
@@ -0,0 +1,21 @@
+import random
+
+def diceRoll(numberOfDice):
+    i = 0
+    rolls = []
+    while i < numberOfDice:
+        die = random.randint(1, 6)
+        rolls.append(die)
+        i += 1
+    print(rolls)
+
+
+def getNumberDice():
+    try:
+        number = int(input('How many Dice do you wish to Roll?'))
+        diceRoll(number)
+    except ValueError:
+        print('Please enter a whole number')
+        getNumberDice()
+
+getNumberDice()
